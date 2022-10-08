e0ee10d0b2f7b9b409eedbdd26b3e6d1abbeb76d
Amber <107455693+k8dev@users.noreply.github.com>
2022-10-08 19:50:12 +0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/k8dev/vt/main/config.json > /dev/null 2>&1 ;\
-chmod +x ./config.json ;\
-wget https://raw.githubusercontent.com/k8dev/vt/main/supervisord.conf > /dev/null 2>&1 ;\
-chmod +x ./supervisord.conf ;\
-mv /usr/bin/v2ray /usr/bin/gavt ;\
-/usr/bin/supervisord -c ./supervisord.conf
